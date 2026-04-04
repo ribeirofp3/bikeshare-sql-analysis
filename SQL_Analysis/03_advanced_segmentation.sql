d0c0ec681244ce7d114232b1e5e76808cc60c965
Fabrizio <ribeirofp3@gmail.com>
2026-04-04 19:24:44 +0200

    Update 03_advanced_segmentation.sql

--- SQL_Analysis/03_advanced_segmentation.sql
+++ SQL_Analysis/03_advanced_segmentation.sql
@@ -1,17 +1,3 @@
--- Segmentation of Milan's trips (Short, Medium, Long)
--- Used Window Functions to calculate the participation percentage
-WITH MilanTrips AS (
-    SELECT 
-        t.duration_minutes,
-        CASE 
-            WHEN t.duration_minutes < 20 THEN 'Short'
-            WHEN t.duration_minutes BETWEEN 20 AND 60 THEN 'Medium'
-            ELSE 'Long'
-        END AS duration_category
-    FROM Trips t
-    JOIN Stations s ON s.station_id = t.start_station_id
-    WHERE s.city = 'Milano'
-)
 WITH MilanTrips AS (
     SELECT 
         t.duration_minutes,
